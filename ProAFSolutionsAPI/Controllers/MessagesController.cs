ca30bab58434da70bae7b827606f2fb9f1608d33
Alex <alexclavijo85@gmail.com>
2016-10-26 11:14:25 -0400

    Adding message sending server side logic

--- ProAFSolutionsAPI/Controllers/MessagesController.cs
+++ ProAFSolutionsAPI/Controllers/MessagesController.cs
@@ -7,6 +7,9 @@ using System.Net.Http;
 using System.Web.Http;
 using ProAFSolutionsAPI.Models;
 using System.Web.Http.Cors;
+using Twilio;
+using System.Diagnostics;
+using System.Net.Mail;
 
 namespace ProAFSolutionsAPI.Controllers
 {
@@ -26,5 +29,45 @@ namespace ProAFSolutionsAPI.Controllers
             //todo: Send email and|or text
             return Ok();
         }
+
+        private void SendSMS(string text, string phoneNumber)
+        {
+            var accountSid = "AC503e8b4cdf8a7fee94e79568bed71651"; // Your Account SID from www.twilio.com/console
+            var authToken = "d853c44bc9035960eedaf51ebc4f1aef";  // Your Auth Token from www.twilio.com/console
+
+            var twilioClient = new TwilioRestClient(accountSid, authToken);
+            var message = twilioClient.SendMessage("+17864754911", phoneNumber, text);
+            if (message.RestException != null)
+            {
+                var error = message.RestException.Message;
+                Debug.WriteLine("Error: " + message);
+            }
+        }
+
+        private void SendEmail(string[] to) {
+
+            using (var client = new SmtpClient()) {
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Credentials = new System.Net.NetworkCredential("proafsolutions@gmail.com", "test123");
+                client.Timeout = 10000;
+
+                var message = new MailMessage();
+                message.From = new MailAddress("proafsolutions@gmail.com", "ProAFSolutions System");
+                message.Body = "Text here!!!";
+                to.ToList().ForEach(A => message.To.Add(A));
+
+                try
+                {
+                    client.Send(message);
+                }
+                catch (Exception ex) {
+                    Debug.WriteLine("Error: " + ex.Message);
+                }                
+            }          
+        }
+         
     }
 }
