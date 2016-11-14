b2dd32a357030e8b4c054e31be58deeb0793c7e9
filyjunior <filyjunior@gmail.com>
2016-11-14 14:01:27 -0500

    Redirect to Contact Message

--- ProAFSolutions/app/components/contact/contact.controller.ts
+++ ProAFSolutions/app/components/contact/contact.controller.ts
@@ -6,22 +6,34 @@
 
     class ContactController implements IContactController {
 
-        static $inject = ['$scope', '$dataContext', '$contactService']; 
+        static $inject = ['$scope', '$dataContext', '$contactService', '$location']; 
 
         public contactMessage: models.IContactMessage;
 
         constructor(private $scope: ng.IScope,
                     protected $dataContext: shared.IDataContextService,
-                    protected $contactService: services.IContactService) {           
+                    protected $contactService: services.IContactService,
+                    protected $location: ng.ILocationService) {           
             this.init();
         }
 
-        init(): void { }
+        init(): void
+        {
+            this.contactMessage = new models.ContactMessage('','','','','');
+            var _self = this;
+            this.$scope.$on('SelectedPackage!', function (events, args) {
+                // console.log(args);
+                _self.$location.url('##contact');
+                _self.contactMessage.subject = args;
+                _self.contactMessage.message = "test";
+            });
+        }
 
         public sendContactMessage(): void {
             //todo: callback 
             this.$contactService.sendMessage(this.contactMessage);
         }
+      
 
        /*function isValidEmail(emailAddress) {
             var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
