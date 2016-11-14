825110f4f98220f13a82b2113a43eace74d9ff69
alexclavijo <alexclavijo85@gmail.com>
2016-11-14 14:28:07 -0500

    App logs setup

--- ProAFSolutionsAPI/Global.asax.cs
+++ ProAFSolutionsAPI/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProAFSolutionsAPI.Providers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,7 +18,9 @@ namespace ProAFSolutionsAPI
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);          
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            LoggerProvider.Info("App Running");         
         }
 
         protected void Application_BeginRequest()
---
+++ ProAFSolutionsAPI/Providers/LoggerProvider.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProAFSolutionsAPI.Providers
+{
+    public static class LoggerProvider
+    {
+
+        static LoggerProvider() {
+            Logger.SetLogWriter(new LogWriterFactory().Create());        
+        }
+
+        public static void WriteLog(LogEntry Entry)
+        {
+            Logger.Write(Entry);            
+        }
+
+        public static void WriteLog(string Message, TraceEventType Type)
+        {
+            WriteLog(new LogEntry
+            {
+                Message = Message,
+                Severity = Type                              
+            });
+        }
+
+        public static void Info(string Message)
+        {
+            WriteLog(Message, TraceEventType.Information);
+        }
+
+        public static void Error(string Message)
+        {
+            WriteLog(Message, TraceEventType.Error);
+        }        
+    }
+}
