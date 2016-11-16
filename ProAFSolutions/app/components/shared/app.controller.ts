7ee2073c851aded2b91bf75ecbac53bfb370e2e8
Alex <alexclavijo85@gmail.com>
2016-11-16 17:20:41 -0500

    text changes across the app

--- ProAFSolutions/app/components/shared/app.controller.ts
+++ ProAFSolutions/app/components/shared/app.controller.ts
@@ -20,7 +20,8 @@
         }
 
         init(): void {
-            this.setupMetadata();           
+            this.setupMetadata();
+            this.customizeChatWindow();         
         }       
 
         private setupMetadata(): void {
@@ -36,18 +37,32 @@
             });
         }
 
-        //public magicShortcut(event: KeyboardEvent): void {
+        public customizeChatWindow(): void{
+
+            setTimeout(() => {
+
+                var $chatIframe = $('#tawkchat-iframe-container').find('#tawkchat-maximized-iframe-element');
+                
+                var $content = $chatIframe.contents();
+                //Removes 'Powered By' 
+                $content.find('#tawktoLink').remove();
+
+                //Changing options menu a little
+                $content.find('#openMenu').text("Chat Settings").css("text-transform", "Capitalize");
+
+            }, 3000);           
+        }
+
+        public openChatWindowClick(): void {
+            var $chatIframe = $('#tawkchat-iframe-container').find('#tawkchat-minified-iframe-element-round');       
+            $chatIframe.contents().find("#tawkchat-status-icon").click();
+        }
+
+         //public magicShortcut(event: KeyboardEvent): void {
         //    if (event.ctrlKey && event.altKey && event.shiftKey && event.keyCode == 13) {
         //        window.open("/admin-chat.html", "AdminChat", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");                          
         //    }
         //}
-
-        public openChatWindowClick(): void {
-            $('#tawkchat-iframe-container').find('#tawkchat-minified-iframe-element-round')
-                                           .contents()
-                                           .find("#tawkchat-status-icon")
-                                           .click();
-        }
 
        
     }
