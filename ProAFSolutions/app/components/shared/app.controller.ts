0df9650a9c619fce3251664cdd6c6631f700cde5
Alain Espaigne <alain.espaigne@gmail.com>
2016-11-17 17:52:15 -0500

    Chat i18n

--- ProAFSolutions/app/components/shared/app.controller.ts
+++ ProAFSolutions/app/components/shared/app.controller.ts
@@ -8,7 +8,7 @@
         //Actions
         catchHttpErrors(): void;
         setupMetadata(): void;
-        customizeChatWindow(): void;   
+        customizeChatWindow(language: string): void;   
     }
 
     class AppController implements IAppController {
@@ -17,8 +17,8 @@
 
         public keywords: string;
         public authors: string;
-        public description: string;
-       
+        public description: string;      
+ 
 
         constructor(private $scope: ng.IScope,
                     private $dataContext: shared.IDataContextService) {          
@@ -28,7 +28,7 @@
         init(): void {
             this.catchHttpErrors();
             this.setupMetadata();
-            this.customizeChatWindow();         
+            this.voidSetupChatWindow();           
         }       
 
         public catchHttpErrors(): void {
@@ -51,21 +51,75 @@
             });
         }
 
-        public customizeChatWindow(): void{
-
-            setTimeout(() => {
-
-                var $chatIframe = $('#tawkchat-iframe-container').find('#tawkchat-maximized-iframe-element');
-                
-                var $content = $chatIframe.contents();
-                //Removes 'Powered By' 
-                $content.find('#tawktoLink').remove();
-
-                //Changing options menu a little
-                $content.find('#openMenu').text("Chat Settings").css("text-transform", "Capitalize");
-
-            }, 3000);           
+        public voidSetupChatWindow(): void {
+            this.$scope.$watch(() => this.$dataContext.currentLanguage, (newValue: string, oldValue: string) => {
+                if (newValue != oldValue) {
+                    let _self = this;
+                    setTimeout(() => {
+                        _self.customizeChatWindow(newValue);
+                    }, 500);
+                }
+            });  
         }
+
+        public customizeChatWindow(language: string): void {      
+
+            var $chatIframe = $('#tawkchat-iframe-container').find('#tawkchat-maximized-iframe-element');
+                
+            var $content = $chatIframe.contents();
+
+            //Removes 'Powered By' 
+            $content.find('#tawktoLink').remove();
+
+            //Changing options menu a little
+            $content.find('#openMenu').text("Chat Settings").css("text-transform", "Capitalize");
+
+            //Options container
+            var optionsContainer = $content.find("#optionsContainer");               
+
+            if (language == 'en-US') {
+                $content.find("#greetingsContainer").text("We are live and ready to chat with you now. Say something to start a live chat.");
+                $content.find("#chatTextarea").attr("placeholder", "Type here and press enter..");                    
+                $content.find("#openMenu").text("Chat Settings");
+                $content.find("#closeMenu").html("Close menu <span></span>");
+                //menu options
+                optionsContainer.find("#endChatOption").find(".optionTitle").text("End chat");
+                optionsContainer.find("#uploadFileOption").find(".optionTitle").text("Upload File");
+                optionsContainer.find("#soundOption").find(".optionTitle").text("Sound On");
+                optionsContainer.find("#emailTranscriptOption").find(".optionTitle").text("Email Transcript");
+                optionsContainer.find("#printOption").find(".optionTitle").text("Print Transcript");
+                optionsContainer.find("#changeNameOption").find(".optionTitle").text("Change Name");
+                //controls titles
+                $content.find("#minimizeChat").attr("title", "Minimize");
+                $content.find("#popoutChat").attr("title", "Pop out");
+                $content.find("#endChat").attr("title", "End Chat");
+                //end chat actions
+                $content.find("#endChatFormMessageContainer").text("Are you sure you want to end this chat?");
+                $content.find("#formCancel").text("Cancel");
+                $content.find("#formSubmit").text("End Chat");                 
+            }
+            else if (language == 'es-ES') {
+                $content.find("#greetingsContainer").text("Estamos listos para chatear con usted. Teclee debajo para iniciar una conversación.");
+                $content.find("#chatTextarea").attr("placeholder", "Teclee aqui y presione enter..");                    
+                $content.find("#openMenu").text("Opciones");  
+                $content.find("#closeMenu").html("Cerrar menú <span></span>");
+                // menu options
+                optionsContainer.find("#endChatOption").find(".optionTitle").text("Terminar Conversación");
+                optionsContainer.find("#uploadFileOption").find(".optionTitle").text("Subir Fichero");
+                optionsContainer.find("#soundOption").find(".optionTitle").text("Activar Sonido");
+                optionsContainer.find("#emailTranscriptOption").find(".optionTitle").text("Enviar por Correo");
+                optionsContainer.find("#printOption").find(".optionTitle").text("Imprimir Conversación");
+                optionsContainer.find("#changeNameOption").find(".optionTitle").text("Cambiar Nombre");
+                //controls titles
+                $content.find("#minimizeChat").attr("title", "Minimizar");
+                $content.find("#popoutChat").attr("title", "Separar");
+                $content.find("#endChat").attr("title", "Cerrar Chat");            
+                // end chat actions
+                $content.find("#endChatFormMessageContainer").text("Está seguro que desea terminar esta conversación?");   
+                $content.find("#formCancel").text("Cancelar");
+                $content.find("#formSubmit").text("Finalizar Chat");                                   
+            }                      
+        }      
 
         //public magicShortcut(event: KeyboardEvent): void {
         //    if (event.ctrlKey && event.altKey && event.shiftKey && event.keyCode == 13) {
